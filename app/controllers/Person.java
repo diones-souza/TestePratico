26b470ba5647e699e995c884dd7fb68ca3324aae
johnny-bitis <51972715+johnny-bitis@users.noreply.github.com>
2020-02-21 00:34:56 -0400

    crud person

--- app/controllers/Person.java
+++ app/controllers/Person.java
@@ -5,6 +5,8 @@ import play.mvc.*;
 
 import java.util.*;
 
+import models.People;
+
 public class Person extends Controller {
 
     public static void index() {
@@ -12,7 +14,25 @@ public class Person extends Controller {
     }
 
     public static void list() {
-        render();
+        List<People> people = People.findAll();
+        render(people);
     }
 
+    public static void save(People person) {
+        person.save();
+        flash.success("Registro Salvo");
+        index();
+    }
+
+    public static void edit(Long id) {
+        People person = People.findById(id);
+        renderTemplate("Person/index.html",person);
+    }
+
+    public static void destroy(Long id) {
+        People person = People.findById(id);
+        person.delete();
+        flash.success("Registro excluido");
+        list();
+    }
 }
